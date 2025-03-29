acc2462315df98d50f442840d8fe2005f344e72b
iuliivasilev <yuliivasiliev@gmail.com>
2025-03-29 17:51:06 +0100

    Added CI/CD & logging

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from src.data_loader import DataLoader
 from src.model import ProjectModel
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+import logging
+
+# Настройка логирования
+logging.basicConfig(filename="training.log", level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
 def main():
@@ -15,17 +19,17 @@ def main():
         if X is None or y is None:
             break  # Если данных больше нет, выходим из цикла
         if i > 0:
-            print("Test on new data")
+            logging.info("Testing on new data")
             y_pred = p_model.predict(X)
             # print(y, y_pred)
-            print(f"R2: {r2_score(y, y_pred)}")
+            logging.info(f"R2: {r2_score(y, y_pred)}")
             
             # print("Coefs")
             # print(data_stream.get_weights())
             # print(p_model.model.coef_)
         p_model.fit(X, y)  # Обучаем модель на текущей порции данных
-        print("Added new data")
-        print(f"Quality on full data: {p_model.score()}")
+        logging.info("Added new data")
+        logging.info(f"Quality on full data: {p_model.score()}")
 
 if __name__ == "__main__":
     main()
